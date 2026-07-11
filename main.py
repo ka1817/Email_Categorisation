f9d9b5b26221af93f51d00c1b61cb9ef4b2a6751
PRANAV_REDDY <reddysaipranav6@gmail.com>
2026-07-11 10:43:05 +0530

    classification

--- main.py
+++ main.py
@@ -23,4 +23,4 @@ X_train_pad,y_train,X_test_pad,y_test,tokenizer=processor.preprocess(df)
 model=processor.train(X_train_pad,y_train)
 print(model.evaluate(X_test_pad,y_test))
 print("Results")
-
+print("Classification Results")
